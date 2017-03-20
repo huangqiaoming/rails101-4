f24ecfdfcfa5008a8cdbc7edbb71862053a7b701
huangqiaoming <carolinehuang.aiesec@gmail.com>
2017-03-20 15:09:45 +0800

    implement group relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_many :groups
   has_many :posts
+  has_many :group_relationships
+  has_many :participated_groups, :through => :group_relationships, :source => :group
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
