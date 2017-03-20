5614870703ed8831bc79b3585ae5fe701ddbc69e
huangqiaoming <carolinehuang.aiesec@gmail.com>
2017-03-20 15:24:14 +0800

    implement is_member_of?(group)

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  def is_member_of?(group)
+    participated_groups.include?(group)
+  end
 end
