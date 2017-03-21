621749d60e5a3ec3ce12f3d16709cb1f3de82527
huangqiaoming <carolinehuang.aiesec@gmail.com>
2017-03-21 19:29:32 +0800

    implement my posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, :only => [:new,:create]
+  before_action :authenticate_user!, :only => [:new,:create, :edit, :update, :destroy]
   def new
     @group = Group.find(params[:group_id])
     @post = Post.new
@@ -17,6 +17,36 @@ class PostsController < ApplicationController
       render :new
     end
   end
+
+  def edit
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    @post.group = @group
+  end
+
+  def update
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    @post.group = @group
+    @post.user = current_user
+
+    if @post.update(post_params)
+
+      redirect_to account_posts_path, notice: "编辑成功"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    @post.group = @group
+    @post.destroy
+    flash[:alert] = "成功删除文章！"
+    redirect_to account_posts_path
+  end
+
   private
 
   def post_params
